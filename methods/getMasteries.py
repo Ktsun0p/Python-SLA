4b361886dde0f56cee99c11ee1fa468b970833e0
Ktsun0p <77063823+Ktsun0p@users.noreply.github.com>
2024-05-25 18:57:16 -0400

    fixed with new masteries

--- methods/getMasteries.py
+++ methods/getMasteries.py
@@ -1,21 +1,22 @@
 import requests
 from methods.getVersion import get_version
 from methods.getChampByKey import get_champ_by_key
-import json
 
 champion_by_id_cache = {}
 champion_json = {}
 
 masteries_emojis = [
-    "<:m1:1160313061106782208>",
-    "<:m2:1160313059001237595>",
-    "<:m3:1160313056102985881>",
-    "<:m4:1160313054098104421>",
-    "<:m5:1160313051489259683>",
-    "<:m6:1160313049937358868>",
-    "<:m7:1160313046883897364>"
-]
-
+    "<:m1:1244057161911373906>",
+    "<:m2:1244057159659159583>",
+    "<:m3:1244057156777410660>",
+    "<:m4:1244057632885575770>",
+    "<:m5:1244057634609565777>",
+    "<:m6:1244057636220174366>",
+    "<:m7:1244057637801169009>",
+    "<:m8:1244057639462375536>",
+    "<:m9:1244057642066772030>",
+    "<:m10:1244057644638146632>"
+    ]
 def get_masteries(summoner_id, region, key, language="en_US"):
     last_ver = get_version()
     masteries = requests.get(f'https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{summoner_id}?api_key={key}').json()
@@ -32,13 +33,13 @@ def get_masteries(summoner_id, region, key, language="en_US"):
             fm1 = mastery['championLevel']
             pfm1 = mastery['championPoints']
             champ_emj = "<:Toto_Bug:1019280617105522729>" if str(mastery['championId']) not in champ_emojis else champ_emojis[str(mastery['championId'])]['icon']
-
+            levelEmj = "<:m10:1244057644638146632>" if (fm1-1) >= 10 else masteries_emojis[fm1 - 1]
             top_masteries.append({
                 'name': fm['name'],
                 'id': fm['id'],
                 'slogan': fm['title'],
                 'level': fm1,
-                'levelEmoji': masteries_emojis[fm1 - 1],
+                'levelEmoji': levelEmj,
                 'emoji': champ_emj,
                 'points': pfm1,
                 'full': f'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{fm["id"]}_0.jpg',
